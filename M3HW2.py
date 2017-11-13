394f1c524a2bcdbe0e5a87de53d3f14386a09051
archdr4g0n <31485926+archdr4g0n@users.noreply.github.com>
2017-11-12 20:58:47 -0500

    Add files via upload

--- M3HW2.py
+++ M3HW2.py
@@ -3,16 +3,45 @@
 ##CSC221
 ## M3HW2
 
-import maze_builder
-##import parser
-maze = maze_builder.OneRoomMaze()
-##parser = parser.parser
+import M3HW1
 
-def main():
-    cell = maze.getCurrentCell()
-    print(cell)
+class Room(object):
+    # create room
+    def __init__(self, name='None'):
+        self.name = name
+        self.next_room = None
+
+    def __str__(self):
+        return "Room: ("+str(self.name)+")"
+    # get room name
+    def get_name(self):
+        return self.name
+    # link next room
+    def next_room(self, room):
+        self.next = room
 
 
+a = Room(1)
+b = Room(2)
+a.next_room(b)
+c = Room(end)
+b.next_room(c)
 
+class Dungeon(object):
+    # create maze object
+    def __init__(self):
+        self.list = []
+        self.currentroom = None
 
-main()
+    def get_currentroom(self):
+        return self.currentroom
+
+    def set_currentroom(self, cell):
+        self.currentroom = cell
+
+Iamat = a
+print(Iamat)
+Iamat = Iamat.pop
+print (Iamat)
+Iamat = Iamat.pop
+print (Iamat)
